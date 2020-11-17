a0a3c64b205cc7eade7d47c51e7ef7a2f1237868
Madara <None>
2020-11-17 18:48:53 +0200

    Change matrix vertical line from dict to VLine class.

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -8,10 +8,11 @@ from typing_extensions import TypedDict
 from .typedefs import TermColor
 
 
-class VLine(TypedDict):
-    empty: bool
-    length: int
-    max_length: int
+class VLine:
+    def __init__(self, is_empty: bool, length: int, max_length: int) -> None:
+        self.is_empty = is_empty
+        self.length = length
+        self.max_length = max_length
 
 
 class Matrix:
@@ -25,7 +26,7 @@ class Matrix:
             width (int): matrix width
             height (int): matrix height
             color (TermColor): matrix color
-            chars (List[str]): characters for construct matrix
+            chars (List[str]): characters for matrix construct
         """
 
         self.width = width
@@ -35,7 +36,7 @@ class Matrix:
         self.max_vline_len = height // 2
         self.chars = chars
         self.vlines: List[VLine] = [
-            {"empty": True, "length": 0, "max_length": 0} for _ in range(width)]
+            VLine(is_empty=True, length=0, max_length=0) for _ in range(width)]
 
     def gen_new_line(self) -> str:
         """Generates new matrix line.
@@ -59,13 +60,13 @@ class Matrix:
             str: new character for vertical line
         """
 
-        vline_len = vline["length"] = vline["length"] + 1
-        if vline_len > vline["max_length"]:
-            vline["empty"] = not vline["empty"]
-            vline["length"] = 1
-            vline["max_length"] = random.randint(
+        vline_len = vline.length = vline.length + 1
+        if vline_len > vline.max_length:
+            vline.is_empty = not vline.is_empty
+            vline.length = 1
+            vline.max_length = random.randint(
                 self.min_vline_len, self.max_vline_len)
-        char = " " if vline["empty"] else self._gen_char()
+        char = " " if vline.is_empty else self._gen_char()
         return char
 
     def _gen_char(self) -> str:
